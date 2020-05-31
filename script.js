f3ed7a138777d3486b94ac1f112c1cf6a286626e
root <root@rg.home>
2020-05-31 09:34:00 -0400

    green

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const boxXelements = Math.floor(boxWidth / elementWidth);
 const boxYelements = Math.floor(boxHeight / elementHeight);
 const boxEnemyCount = 5;
 const boxFriendCount = 50;
-const backGroundColor = "yellow";
+const backGroundColor = "green";
 var cvs = document.createElement('canvas');
   
 // load audio files 
