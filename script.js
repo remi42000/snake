0dade5753b62ecbf2369961f86e316856a0b85e9
root <root@rg.home>
2020-05-31 06:39:14 -0400

    11

--- script.js
+++ script.js
@@ -254,7 +254,7 @@ function draw(recursive = false, isFriend = null) {
     ctx.font = "30px Arial";
     ctx.strokeText("Score: " + score, 10, 50);
 
-    if (score >= 10) {
+    if (score >= 11) {
         gameSpeed = 50;
      //   clearInterval(game);
      //   game = setInterval(draw, gameSpeed);
